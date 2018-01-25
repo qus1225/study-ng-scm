3fe3c05e4a64688009d581a3e74ba0cb6c3ee740
qus1225 <qus1225@gmail.com>
2018-01-26 01:36:10 +0900

    Add navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  appTitle = '상품관리 시스템';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  searchProduct(no: number) {
+    console.log(`search: ${no}`);
   }
 
 }
