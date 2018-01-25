ede47c79410e42b564c59d2d57eb945c538c336e
qus1225 <qus1225@gmail.com>
2018-01-26 02:09:57 +0900

    Change main page when selected sidebar menu changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SidebarMenu } from './sidebar/sidebar.component'
 
 @Component({
   selector: 'scm-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'scm';
+  currentMenu: SidebarMenu;
 }
