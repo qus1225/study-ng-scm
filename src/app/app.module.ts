b281b0e216c55aa3518337c1c3f1a68df59f2e25
qus1225 <qus1225@gmail.com>
2018-01-26 20:49:13 +0900

    Add core modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ProductModule } from './product/product.module';
 import { CategoryModule} from './category/category.module';
+import { ScmMainModule } from './scm-main/scm-main.module';
 
 
 import { AppComponent } from './app.component';
@@ -9,21 +10,19 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    SidebarComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     // Angular Modules
-    BrowserModule, FormsModule,
+    BrowserModule,
+    FormsModule,
     // App Modules
-    ProductModule, CategoryModule
+    ScmMainModule,
+    ProductModule,
+    CategoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
