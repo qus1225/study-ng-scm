5e482257f0fab0f7d6212e1e7657a6bf41988d1f
qus1225 <qus1225@gmail.com>
2018-01-26 20:43:18 +0900

    Add function modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ProductModule } from './product/product.module';
+import { CategoryModule} from './category/category.module';
 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 
 @NgModule({
@@ -16,7 +20,10 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    // Angular Modules
+    BrowserModule, FormsModule,
+    // App Modules
+    ProductModule, CategoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
